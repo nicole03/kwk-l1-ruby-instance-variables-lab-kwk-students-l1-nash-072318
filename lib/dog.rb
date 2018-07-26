bef9669f800d767978686f4bb14ab87d0224ba6d
nicole03 <ntidwell03@gmail.com>
2018-07-26 21:04:46 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
 # dog.rb
 class Dog 
-  def name=(dog_name)
+  def name=(this_dogs_name)
     this_dogs_name = dog_name
   end 
   
@@ -12,3 +12,4 @@ end
 
 lassie = Dog.new 
 lassie.name = "Lassie"
+
